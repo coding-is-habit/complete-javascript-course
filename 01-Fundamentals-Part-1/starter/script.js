f07343a5dd6987e042aa12279f722c822528f76b
Madhav Sharma <madhav9620@gmail.com>
2023-02-15 23:31:01 +0530

    learn variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,44 @@
+/*
 let js = "amazing";
 if (js === 'amazing') alert("JavaScript is FUN!");
 
 console.log(48 + 10 + 23 - 10);
+
+
+
+// if variable is going to get mutate then we use var
+let age = 30;
+age = 31;
+
+// const we use use when we can't mutate
+const birthYear = 1999;
+
+// var is prior to es6
+var job = 'programmer'
+job = 'teacher'
+
+
+// Mathematical operators
+const ageJonas = 2037 -1991;
+const ageSarah = 2037-2018;
+console.log(ageJonas, ageSarah)
+
+
+let markHeight = 1.69;
+let markWeight = 78;
+let johnHeight = 1.88;
+let johnWeight = 95;
+
+markBMI = markWeight/(markHeight**2);
+johnBMI = johnWeight/(johnHeight**2);
+let markHigherBMI = markBMI > johnBMI
+
+console.log(markBMI, johnBMI)
+console.log("mark higher bmi", markHigherBMI)
+*/
+
+const firstName = "Jonas";
+const birthYear = 1991;
+const job = "teacher";
+const year = 2037;
+
